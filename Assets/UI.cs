6fdc64ca56457cf8c6b11616470a80ec15eb665b
Arto Koistinen <arzi42@gmail.com>
2014-04-12 16:53:26 +0300

    UI
    UI component

---
+++ Assets/UI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour 
+{
+	private int moves = 0;
+
+	public static UI instance;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	public void Moved()
+	{
+		moves ++;
+	}
+
+	void OnGUI()
+	{
+		GUI.Box(new Rect(10, 10, 70, 30), "");
+
+		GUI.Label(new Rect(20, 15, 90, 20), "Moves: " + moves);
+	}
+
+}
